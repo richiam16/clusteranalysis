fd824d48219a512b4c0658b957fcdd0b85158745
Ricky Sexton <risexto878@gmail.com>
2024-06-25 17:02:16 -0700

    added ramachandran test

--- clusterkit/tests/test_clusterkit.py
+++ clusterkit/tests/test_clusterkit.py
@@ -8,7 +8,7 @@ import pytest
 import sys
 from MDAnalysis.tests.datafiles import PSF, DCD
 import MDAnalysis as mda
-
+import numpy as np
 
 def test_clusterkit_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -21,9 +21,21 @@ def test_mdanalysis_logo_length(mdanalysis_logo_text):
     assert len(logo_lines) == 46, "Logo file does not have 46 lines!"
 
 
-def test_get_features_ramachandran():
+def test_get_features_ramachandran_shape():
     from clusterkit.ClusterAnalysis import ClusterAnalysis
     u = mda.Universe(PSF, DCD)
     C = ClusterAnalysis(u)
     C.featurize('ramachandran')
     assert C.data.shape == (98, 848)
+
+
+def test_get_features_ramachandran():
+    from clusterkit.ClusterAnalysis import ClusterAnalysis
+                                            
+    u = mda.Universe(PSF, DCD)
+    C = ClusterAnalysis(u)
+    C.featurize('ramachandran')
+    data = np.load('clusterkit/data/ramachandran.npy')
+    assert (C.data == data).all()
+
+
